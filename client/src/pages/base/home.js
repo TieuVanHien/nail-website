f4d656885643181ea1ff1cafe08929dd28670a41
Van Hien <hiench2001@gmail.com>
2023-09-18 14:14:47 -0600

    updated home section

--- client/src/pages/base/home.js
+++ client/src/pages/base/home.js
@@ -5,8 +5,10 @@ export const Home = () => {
     <section className="home" id="home">
       <div className="container">
         <div className="heading">
-          <h1>Discover the Artistry of </h1>
-          <h1 style={{ marginLeft: "4em" }}>Nail Elegance at Our Salon</h1>
+          <h1 className="head1">Discover the Artistry of </h1>
+          <h1 className="head2" style={{ marginLeft: "4em" }}>
+            Nail Elegance at Our Salon
+          </h1>
         </div>
         <div className="home-img">
           <img src={home} alt="home page background" />
