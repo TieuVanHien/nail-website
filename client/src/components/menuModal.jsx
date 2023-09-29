47a0cf78cf422747528227a56840bd2b66763767
Van Hien <hiench2001@gmail.com>
2023-09-29 11:26:37 -0600

    updated the nail examples

--- client/src/components/menuModal.jsx
+++ client/src/components/menuModal.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 import Modal from "react-modal";
-// import { menu } from "../images";
+import {
+  ex1,
+  ex2,
+  ex3,
+  ex4,
+  ex5,
+  ex6,
+  ex7,
+  ex8,
+  ex9,
+  ex10,
+  ex11,
+  ex12,
+  ex13,
+  ex14,
+  ex15,
+  ex16,
+  ex17,
+  ex18,
+  ex19,
+  ex20,
+  ex21,
+  ex22,
+} from "../images/index";
 
 export const MenuModal = ({ OpenModal }) => {
   return (
@@ -11,8 +34,29 @@ export const MenuModal = ({ OpenModal }) => {
       className="mod"
       overlayClassName="over-lay"
     >
-      <div className="menu">
-        {/* <img className="menu" src={menu} alt="menu" /> */}
+      <div className="nail-examples">
+        <img className="image" src={ex1} alt="Nail 1" />
+        <img className="image" src={ex2} alt="Nail 2" />
+        <img className="image" src={ex3} alt="Nail 3" />
+        <img className="image" src={ex4} alt="Nail 4" />
+        <img className="image" src={ex5} alt="Nail 5" />
+        <img className="image" src={ex6} alt="Nail 6" />
+        <img className="image" src={ex7} alt="Nail 7" />
+        <img className="image" src={ex8} alt="Nail 8" />
+        <img className="image" src={ex9} alt="Nail 9" />
+        <img className="image" src={ex10} alt="Nail 10" />
+        <img className="image" src={ex11} alt="Nail 11" />
+        <img className="image" src={ex12} alt="Nail 12" />
+        <img className="image" src={ex13} alt="Nail 13" />
+        <img className="image" src={ex14} alt="Nail 14" />
+        <img className="image" src={ex15} alt="Nail 15" />
+        <img className="image" src={ex16} alt="Nail 16" />
+        <img className="image" src={ex17} alt="Nail 17" />
+        <img className="image" src={ex18} alt="Nail 18" />
+        <img className="image" src={ex19} alt="Nail 19" />
+        <img className="image" src={ex20} alt="Nail 20" />
+        <img className="image" src={ex21} alt="Nail 21" />
+        <img className="image" src={ex22} alt="Nail 22" />
       </div>
     </Modal>
   );
--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,3 +1,26 @@
+import ex1 from "./nail-1.jpg";
+import ex2 from "./nail-2.jpg";
+import ex3 from "./nail-3.jpg";
+import ex4 from "./nail-4.jpg";
+import ex5 from "./nail-5.jpg";
+import ex6 from "./nail-6.jpg";
+import ex7 from "./nail-7.jpg";
+import ex8 from "./nail-8.jpg";
+import ex9 from "./nail-9.jpg";
+import ex10 from "./nail-10.jpg";
+import ex11 from "./nail-11.jpg";
+import ex12 from "./nail-12.jpg";
+import ex13 from "./nail-13.jpg";
+import ex14 from "./nail-14.jpg";
+import ex15 from "./nail-15.jpg";
+import ex16 from "./nail-16.jpg";
+import ex17 from "./nail-17.jpg";
+import ex18 from "./nail-18.jpg";
+import ex19 from "./nail-19.jpg";
+import ex20 from "./nail-20.jpg";
+import ex21 from "./nail-21.jpg";
+import ex22 from "./nail-22.jpg";
+
 export const home = require("./home-background.png");
 export const about = require("./about-us.jpg");
 export const nailEx1 = require("./nail-ex1.jpg");
@@ -22,3 +45,28 @@ export const nails9 = require("./nails-9.jpg");
 export const nails10 = require("./nails-10.jpg");
 export const nails11 = require("./nails-11.jpg");
 export const location = require("./location.jpg");
+
+export {
+  ex1,
+  ex2,
+  ex3,
+  ex4,
+  ex5,
+  ex6,
+  ex7,
+  ex8,
+  ex9,
+  ex10,
+  ex11,
+  ex12,
+  ex13,
+  ex14,
+  ex15,
+  ex16,
+  ex17,
+  ex18,
+  ex19,
+  ex20,
+  ex21,
+  ex22,
+};
