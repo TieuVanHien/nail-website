2475cd2ea51682072d140d9999ac42bec7bb3618
Van Hien <hiench2001@gmail.com>
2023-10-04 11:42:44 -0600

    updated contact information

--- client/src/components/modal/contactModal.jsx
+++ client/src/components/modal/contactModal.jsx
@@ -22,7 +22,7 @@ export const ContactModal = ({ OpenModal }) => {
           </div>
           <div className="mod-box">
             <div className="mod-phone">
-              <p>Phone: (888) 888-8888</p>
+              <p>Phone: +1 (403) 921 9207 </p>
               <div className="line-line"></div>
             </div>
             <div className="mod-email">
@@ -30,7 +30,7 @@ export const ContactModal = ({ OpenModal }) => {
                 {" "}
                 Email <br />
                 <Link className="email-link" onClick={() => handleOpen(true)}>
-                  caravelle.reservation@gmail.com
+                  queenslandnailsandspa@gmail.com
                 </Link>
                 {open && <ModalEmail OpenModal={handleOpen} />}
               </p>
