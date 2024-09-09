210d51e1b741aeb30170c28714830c5cf8d98362
Van Hien <hiench2001@gmail.com>
2024-09-08 19:50:38 -0600

    feat: added temporary notification popup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import closed from "./images/closed.png";
+import { AiOutlineClose } from "react-icons/ai";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Nav, Footer } from "./components";
 import { Home, About, Gallery, Reservation, Service } from "./pages";
 
 function App() {
+  const [showPopup, setShowPopup] = useState(true);
+  const handlePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
     <>
       <Router>
+        <div className="closed">
+          {showPopup && (
+            <div className="popup">
+              <AiOutlineClose className="close-icon" onClick={handlePopup} />
+              <img src={closed} alt="temporary closed" />
+            </div>
+          )}
+        </div>
         <Nav />
         <Home />
         <About />
